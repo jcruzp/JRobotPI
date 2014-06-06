20b120d28e5bcc1485cda9888b9c8d8ad1907652
Jose Cruz <>
2014-06-06 08:53:52 -0430

    Set MIT License

--- src/com/jcruz/jrobotpi/i2c/BMP180.java
+++ src/com/jcruz/jrobotpi/i2c/BMP180.java
@@ -1,11 +1,25 @@
-/*
- * Based in Oracle Article How to Become an Embedded Developer in Minutes
- * by Angela Caicedo http://www.oracle.com/technetwork/articles/java/ma14-new-to-java-embedded-2177732.html
- * 
- * And 
+/* 
+ * The MIT License
  *
- * Beyond Beauty: JavaFX, I2C, Parallax, Touch, Raspberry Pi, Gyroscopes and Much More. (Part I)
- * Angela Blog https://blogs.oracle.com/acaicedo/entry/beyond_beauty_javafx_i2c_parallax
+ * Copyright 2014 Jose Cruz <joseacruzp@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package com.jcruz.jrobotpi.i2c;
 
@@ -13,6 +27,14 @@ import jdk.dio.i2cbus.I2CDevice;
 
 /**
  * Control BMP180 pressure and temperature sensor
+ * 
+ * Based in Oracle Article How to Become an Embedded Developer in Minutes
+ * by Angela Caicedo http://www.oracle.com/technetwork/articles/java/ma14-new-to-java-embedded-2177732.html
+ * 
+ * And 
+ *
+ * Beyond Beauty: JavaFX, I2C, Parallax, Touch, Raspberry Pi, Gyroscopes and Much More. (Part I)
+ * Angela Blog https://blogs.oracle.com/acaicedo/entry/beyond_beauty_javafx_i2c_parallax
  *
  * @author jcruz
  */
