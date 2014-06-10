80c8807694365ff86e12db97bf09cb521c78a6fe
eve0002474 <eve0002474@e00jcomp1.epa.com>
2014-06-10 12:33:34 -0430

    SC16IS750 interface

---
+++ src/com/jcruz/jrobotpi/i2c/SC16IS750.java
@@ -0,0 +1,112 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2014 Jose Cruz <joseacruzp@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.jcruz.jrobotpi.i2c;
+
+import jdk.dio.i2cbus.I2CDevice;
+
+/**
+ *
+ * @author jcruz
+ */
+public enum SC16IS750 {
+   
+    /**
+     *
+     */
+    XHR(0x00),
+
+    /**
+     *
+     */
+    FCR(0x02),
+
+    /**
+     *
+     */
+    LCR(0x03),
+
+    /**
+     *
+     */
+    MCR(0x04),
+    
+    /**
+     *
+     */
+    LSR(0x05),
+    
+    /**
+     *
+     */
+    MSR(0x06),
+    
+    /**
+     *
+     */
+    TXLVL(0x08),
+    
+    /**
+     *
+     */
+    RXLVL(0x09),
+    
+    /**
+     *
+     */
+    DLL(0x00),
+    
+    /**
+     *
+     */
+    DLH(0x01);
+    
+    /**
+     *
+     */
+    public int cmd;
+
+    private SC16IS750(int cmd) {
+        // SC16IS740 expects a R/W  bit first (only for spi interface), 
+        //followed by the 4 bit register address of the byte.
+        // So shift the bits left by three bits:
+        this.cmd = cmd << 3;
+    }
+    /**
+     *
+     * @param device
+     * @return
+     */
+    public int read(I2CDevice device) {
+        return I2CUtils.read(device, this.cmd);
+    }
+
+    /**
+     *
+     * @param device
+     * @param value
+     */
+    public void write(I2CDevice device, byte value) {
+        I2CUtils.write(device, (byte) this.cmd, value);
+    }
+}
---
+++ src/com/jcruz/jrobotpi/i2c/driver/SC16IS750Device.java
@@ -0,0 +1,123 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Jose Cruz <joseacruzp@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.jcruz.jrobotpi.i2c.driver;
+
+import com.jcruz.jrobotpi.i2c.I2CRpi;
+import com.jcruz.jrobotpi.i2c.I2CUtils;
+import com.jcruz.jrobotpi.i2c.SC16IS750;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jdk.dio.DeviceConfig;
+import jdk.dio.DeviceManager;
+import jdk.dio.i2cbus.I2CDevice;
+import jdk.dio.i2cbus.I2CDeviceConfig;
+
+/**
+ * Interface to SC16IS750 I2C/SPI to UART I use I2C interface to connect Emic-2
+ * to I2C 0x90 address and free a UART port to use with GPS.
+ *
+ * @author Jose Cruz <joseacruzp@gmail.com>
+ */
+public class SC16IS750Device extends I2CRpi {
+
+    private static final int SC16IS750_write = 0x90;
+    private final I2CDeviceConfig configRead;
+    private final I2CDevice deviceRead;
+
+    /**
+     *
+     * @throws IOException
+     */
+    public SC16IS750Device() throws IOException {
+        super(SC16IS750_write);
+
+        configRead = new I2CDeviceConfig(DeviceConfig.DEFAULT,
+                SC16IS750_write + 1,
+                DeviceConfig.DEFAULT,
+                DeviceConfig.DEFAULT);
+
+        deviceRead = (I2CDevice) DeviceManager.open(I2CDevice.class, configRead);
+
+        configUARTregs();
+    }
+
+    private void configUARTregs() {
+        //Config UART
+        Logger.getGlobal().log(Level.INFO, "Config UART of SC16IS750");
+
+        //Line Control Register: Enable Writing DLH & DLL
+        //& set no Parity, 1 stop bit, and 8 bit word length
+        SC16IS750.LCR.write(device, (byte) 0b10000011);
+
+        //Division registers DLL & DLH
+        // Write '96' to get 9600 baud rate
+        //Assumes you have the version with the ~14MHz crystal
+        // (16x9600 = 153600 = 14.7456Mhz/96)
+        SC16IS750.DLL.write(device, (byte) 96);
+        SC16IS750.DLH.write(device, (byte) 00);
+
+        //Line Control Register: Disnable Writing DLH & DLL
+        //Same setup 
+        SC16IS750.LCR.write(device, (byte) 0b00000011);
+
+        //Modem Control Register
+        //Normal Operating Mode
+        SC16IS750.MCR.write(device, (byte) 0b00000000);
+
+        //FIFO Control Register: Enable the FIFO and no other features
+        SC16IS750.FCR.write(device, (byte) 0b00000111);
+
+        I2CUtils.I2Cdelay(2000);
+
+        Logger.getGlobal().log(Level.INFO, "FIFO Control Register (C1): {0}", String.valueOf(SC16IS750.FCR.read(deviceRead)));
+
+        Logger.getGlobal().log(Level.INFO, "Line Control Register(3): {0}", String.valueOf(SC16IS750.LCR.read(deviceRead)));
+
+        Logger.getGlobal().log(Level.INFO, "Modem Control Register(0): {0}", String.valueOf(SC16IS750.MCR.read(deviceRead)));
+
+    }
+
+    /**
+     *
+     * @param cad String to send to Emic-2
+     */
+    public void write(String cad) {
+        cad = cad.concat("\r\n");
+        ByteBuffer buffer = ByteBuffer.allocateDirect(cad.length());
+        buffer.put(cad.getBytes());
+        buffer.clear();
+//        try {
+//            device.write(SC16IS750.XHR.cmd, 1 ,buffer);
+//        } catch (IOException ex) {
+//            Logger.getGlobal().log(Level.WARNING,ex.getLocalizedMessage());
+//        }
+
+        for (int i = 0; i < cad.length() - 1; i++) {
+            SC16IS750.XHR.write(device, buffer.get(i));
+        }
+
+    }
+}
